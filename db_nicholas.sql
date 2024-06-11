c7eb7a82a9c4983790d69bdabc99a71aef395b9c
Nicholas Musa S S <141598735+Requereves@users.noreply.github.com>
2024-06-11 14:47:21 +0800

    Database

---
+++ db_nicholas.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 11, 2024 at 08:46 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.1.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_nicholas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+-- Error reading structure for table db_nicholas.admin: #1932 - Table &#039;db_nicholas.admin&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_nicholas.admin: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_nicholas`.`admin`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tamu`
+--
+-- Error reading structure for table db_nicholas.tamu: #1932 - Table &#039;db_nicholas.tamu&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_nicholas.tamu: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_nicholas`.`tamu`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
